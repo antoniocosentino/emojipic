f973748d09e5c49c042bd939416c05cb0c660f6f
Antonio Cosentino <hello@antoniocosentino.it>
2023-10-13 19:18:11 +0100

    project init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="mt-5">
+        <h1 className="font-bold text-5xl">hello world</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
